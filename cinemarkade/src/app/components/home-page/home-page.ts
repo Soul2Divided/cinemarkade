40b0ea70c8d8d8e7d37aa2792ab1e92e4801671b
Gabriel Rojas <gaby-ale2002@hotmail.com>
2026-09-10 20:15:45 -0300

    feature: responsive homepage añadida

--- cinemarkade/src/app/components/home-page/home-page.ts
+++ cinemarkade/src/app/components/home-page/home-page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Navbar } from "../navbar/navbar";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Pelicula } from '../../models/pelicula';
+import { HeroItem } from '../../models/hero-item';
+import { ELEMENTOS_HERO_MOCK, LISTA_PELICULAS_MOCK, PELICULA_INTERSTELLAR_MOCK } from '../../data/movies.mock';
+
 
 interface Movie {
   id: number;
@@ -13,47 +16,55 @@ interface Movie {
 }
 
 @Component({
-  imports: [RouterLink, Navbar],
+  imports: [Navbar],
   selector: 'app-home-page',
   styleUrl: './home-page.scss',
   templateUrl: './home-page.html',
 })
 export class HomePage {
-  isAuthModalOpen: boolean = false;
-  modalView: 'options' | 'guest' = 'options';
+  indiceHeroActual: number = 0;
+  private intervaloAutoPlay: any;
 
-  movies: Movie[] = [
-    {
-      id: 1,
-      title: 'INTERSTELLAR',
-      genre: 'SCI-FI',
-      duration: '169 MIN',
-      hall: 'SALA 1 - IMAX',
-      price: 4500,
-      posterUrl: 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg'
-    },
-    {
-      id: 2,
-      title: 'THE BATMAN',
-      genre: 'ACCIÓN',
-      duration: '176 MIN',
-      hall: 'SALA 2 - 3D',
-      price: 4000,
-      posterUrl: 'https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9325a3S.jpg'
+  elementosHero: HeroItem[] = ELEMENTOS_HERO_MOCK;
+  peliculaDestacada: Pelicula = PELICULA_INTERSTELLAR_MOCK;
+  listaPrueba: Pelicula[] = LISTA_PELICULAS_MOCK;
+  generos: string[] = ['CIENCIA FICCIÓN', 'THRILLER', 'DRAMA', 'ACCIÓN'];
+
+  ngOnInit(): void {
+    this.iniciarAutoPlay();
+  }
+
+  ngOnDestroy(): void {
+    this.detenerAutoPlay();
+  }
+
+  iniciarAutoPlay(): void {
+    this.intervaloAutoPlay = setInterval(() => {
+      this.siguienteHero();
+    }, 5000);
+  }
+
+  detenerAutoPlay(): void {
+    if (this.intervaloAutoPlay) {
+      clearInterval(this.intervaloAutoPlay);
     }
-  ];
-
-  openAuthModal(): void {
-    this.modalView = 'options';
-    this.isAuthModalOpen = true;
   }
 
-  closeAuthModal(): void {
-    this.isAuthModalOpen = false;
+  siguienteHero(): void {
+    this.indiceHeroActual = (this.indiceHeroActual + 1) % this.elementosHero.length;
   }
 
-  selectMovie(movie: Movie): void {
-    // Si el usuario intenta comprar sin autenticarse, despliega el modal
-    this.openAuthModal();
+  seleccionarHero(indice: number): void {
+    this.indiceHeroActual = indice;
+    this.detenerAutoPlay();
+    this.iniciarAutoPlay();
+  }
+
+  seleccionarPelicula(pelicula: Pelicula): void {
+    console.log('Película seleccionada:', pelicula.nombre);
+  }
+
+  ejecutarAccionHero(item: HeroItem): void {
+    console.log('Acción ejecutada:', item.titulo);
   }
 }
---
+++ cinemarkade/src/app/models/hero-item.ts
@@ -0,0 +1,10 @@
+export interface HeroItem {
+    id: number;
+    tipo: 'pelicula' | 'combo';
+    etiqueta: string;
+    titulo: string;
+    meta: string;
+    sinopsis: string;
+    imagenBanner: string;
+    textoBoton: string;
+}
